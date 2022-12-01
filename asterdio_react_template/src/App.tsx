88dfcd4d42a3b67e2298bb5822ba54f68e2ec044
susmitahkhanal <susmita.khanal@asterdio.com>
2022-12-01 10:24:14 +0545

    set-up[complete]

--- asterdio_react_template/src/App.tsx
+++ asterdio_react_template/src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "../src/styles/main.scss";
-import Home from "./components/Home";
+import Home from "./Components/Home";
 
 function App() {
     return (
--- asterdio_react_template/src/components/Home.tsx
+++ asterdio_react_template/src/components/Home.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home = () => {
-    return <div className="container">skdjfhfdsjsfdksdf</div>;
+    return <div className="container">React Template</div>;
 };
 
 export default Home;
